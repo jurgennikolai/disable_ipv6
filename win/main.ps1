d0074184f3776d98c5e1995c67b4ac5b321890b6
jurgennikolai <jurgennikolai@gmail.com>
2024-04-03 14:28:01 -0500

    first commit

---
+++ win/main.ps1
@@ -0,0 +1,38 @@
+﻿
+$rootPath = 'C:\ruta\del\script';
+$listSRVs = Get-Content -Path "$rootPath\servers.txt";
+
+$creds = Get-Credential;
+
+foreach($srv in $listSRVs)
+{
+    $session = New-PSSession -ComputerName $srv -Credential $creds;
+    try
+    {
+        $networkInterfaces = Invoke-Command -Session $session -ScriptBlock {
+            Get-NetAdapter;
+        } -ErrorAction Stop;
+        
+        foreach ($interface in $networkInterfaces) {
+            
+            $ipv6before = Invoke-Command -Session $session -ScriptBlock {
+                param($interface);Get-NetAdapterBinding -Name $interface.Name -ComponentID "ms_tcpip6";
+            } -ErrorAction Stop -ArgumentList $interface;
+
+            if($ipv6before.Enabled){
+                $ipv6after = Invoke-Command -Session $session -ScriptBlock {
+                    param($interface);Disable-NetAdapterBinding -Name $interface.Name -ComponentID "ms_tcpip6" -PassThru
+                } -ErrorAction Stop -ArgumentList $interface;
+            }
+        }
+    }
+    catch
+    {
+        $_; 
+    }
+    finally {
+        Remove-PSSession $session;
+    }
+}
+
+
